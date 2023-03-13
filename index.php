955593e324655c2b6b9ef35f9c0ad24bc7c870d8
Danny <danny20854@gmail.com>
2023-03-13 13:03:10 -0400

    update:scrolling

--- index.php
+++ index.php
@@ -37,7 +37,7 @@
 <section class="msger">
     <header class="msger-header">
         <div class="msger-header-title">
-            <i class="fas fa-comments"></i>hiSESSION ID: <input type="text" id="id" hidden> <span class="id_session"></span>
+            <i class="fas fa-comments"></i>SESSION ID: <input type="text" id="id" hidden> <span class="id_session"></span>
         </div>
         <div class="msger-header-options">
             <button id="delete-button">Delete History</button>
